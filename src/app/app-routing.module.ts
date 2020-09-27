7dcaedd6277188839df10a6b0d13818f323d5be6
Jhoppel <jeannine.imhasly@gmail.com>
2020-09-27 18:01:52 +0200

    task 21

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuctionListComponent } from './auction-list/auction-list.component';
-import { AuctionDetailComponent } from './auction-detail/auction-detail.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/auctions' },
-  { path: 'auctions', component: AuctionListComponent },
-  { path: 'auctions/:id', component: AuctionDetailComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'auctions', loadChildren: () => import('./auction/auction.module').then(m => m.AuctionModule) },
+  { path: '', pathMatch: 'full', redirectTo: '/home' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AngularDateHttpInterceptorService } from './shared/angular-date-http-interceptor.service';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AuctionListComponent } from './auction-list/auction-list.component';
-import { MouseEventDisplayComponent } from './mouse-event-display/mouse-event-display.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { AuctionListDetailComponent } from './auction-list-detail/auction-list-detail.component';
-import { AuctionDataService} from './shared/auction-data.service';
-import { AngularDateHttpInterceptorService} from './shared/angular-date-http-interceptor.service';
-import { AuctionDetailComponent } from './auction-detail/auction-detail.component';
-import { HelperService} from './shared/helper.service';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuctionListComponent,
-    MouseEventDisplayComponent,
-    AuctionListDetailComponent,
-    AuctionDetailComponent
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule
   ],
@@ -29,7 +25,7 @@ import { HelperService} from './shared/helper.service';
       provide: HTTP_INTERCEPTORS,
       useClass: AngularDateHttpInterceptorService,
       multi: true
-  }, AuctionDataService, HelperService],
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
